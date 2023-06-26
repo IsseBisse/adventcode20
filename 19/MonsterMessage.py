e25439bff98e4e3222fd195f29ea6c49429d6034
Isak Liljequist <isak.liljequist@cgi.com>
2023-06-26 09:22:02 +0200

    Restart 19

--- 19/MonsterMessage.py
+++ 19/MonsterMessage.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass, field
 import re
+from typing import List, TypeVar
 
 """
 Data parsing
@@ -29,111 +31,68 @@ def parse_rule(string):
 def rule_sorter(string):
 	return int(re.findall(r"^[0-9]+", string)[0])
 
-"""
-Node class
-"""
-class RuleNode:
-	def __init__(self, value):
-		self.value = value
 
-	def __str__(self):
-		return f"Node with value {self.value}"
+def check_group(rule_group, string):
+	rest_string = string
+	for rule_step in rule_group:
+		step_valid, rest_string = rule_step.is_valid(rest_string)
+		if not step_valid:
+			return None
 
-	def add_children(self, children):
-		self.children = children
-
-	def has_value(self):
-		return self.value is not None
-
-	def get_value(self):
-		return self.value
-
-	def generate_value(self):
-		if self.has_value():
-			return True
-
-		children_strings = list()
-		for sub_children in self.children:
-			
-			sub_children_strings = list()
-			for child in sub_children:
-				if child.has_value():
-					sub_children_strings.append(child.get_value())
-
-				else:
-					return False
-
-			children_strings.append("".join(sub_children_strings))
-
-		self.value = "(%s)" % "|".join(children_strings)
-		return True
-
-	# # TODO: flip this upside down (i.e. start from node with only chars)
-	# def get_regex(self):
-	# 	if self.value is not None:
-	# 		return self.value
-
-	# 	else:
-	# 		substrings = list()
-	# 		for sub_children in self.children:
-	# 			substrings.append("".join([child.get_regex() for child in sub_children]))
-
-	# 		return "(%s)" % "|".join(substrings)
+	return rest_string
 
 
-	@staticmethod
-	def generate_children(rule_nodes, rule):
-		children = list()
-		for i, outer_rule in enumerate(rule):
-			sub_children = list()
+@dataclass
+class Node:
+	name: str
+	
 
-			for j, inner_rule in enumerate(outer_rule):
-				sub_children.append(rule_nodes[inner_rule])
+RuleNodeType = TypeVar("RuleNode")
+@dataclass
+class SubNode(Node):
+	steps: List[RuleNodeType] = field(default_factory=lambda: list())
 
-			children.append(sub_children)
+	def step_strings_to_nodes(self, rules):
+		step_strings = self.steps
+		self.steps = list()
+		for string in step_strings:
+			if "\"" in string:
+				self.steps.append(la)
 
-		return children
+			else:
+				self.steps.append(rules[string])
 
-def pattern_matcher(pattern):
-	def match_pattern(string):
-		return re.search(pattern, string) is not None
 
-	return match_pattern
+SubNodeType = TypeVar("SubNode")
+@dataclass
+class RuleNode(Node):
+	groups: List[SubNodeType] = field(default_factory=lambda: list())
+
 
 def part_one():
-	rules_string, received_messages = get_data("input.txt")
-	rules_string.sort(key=rule_sorter)
-	parsed_rules = list(map(parse_rule, rules_string))
+	rule_strings, received_messages = get_data("smallInput.txt")
+	print(rule_strings, received_messages)
 
-	# Create rule nodes	
-	rule_nodes = list()
-	for rule in parsed_rules:
-		value = rule if isinstance(rule, str) else None 
-		node = RuleNode(value)
-		rule_nodes.append(node)
+	rules = dict()
+	sub_rules = dict()
+	for string in rule_strings:
+		num, rule_string = string.split(": ")
+		rules[num] = RuleNode(num)
 
-	# Add children to rule nodes
-	for i, rule in enumerate(parsed_rules):
-		if not isinstance(rule, str):
-			children = RuleNode.generate_children(rule_nodes, rule)
-			rule_nodes[i].add_children(children)
+		for idx, sub_rule_string in enumerate(rule_string.split(" | ")):
+			name = f"{num}.{idx}"
+			sub_rule = SubNode(name)
+			sub_rule.steps = sub_rule_string.split(" ")
+			sub_rules[name] = sub_rule
+			rules[num].groups.append(sub_rule)
 
-	# Generate pattern
-	all_values_set = False
-	has_value = [False] * len(rule_nodes)
-	while not all_values_set:
-		for i, node in enumerate(rule_nodes):
-			has_value[i] = node.generate_value()
+	for sub_rule in sub_rules.values():
+		sub_rule.step_strings_to_nodes(rules)
 
-		print("%d of %d has value" % (sum(has_value), len(rule_nodes)))
-		all_values_set = all(has_value)
-			
-	# Filter messages
-	pattern = "^%s$" % rule_nodes[0].get_value()
-	matcher = pattern_matcher(pattern)
-	correct_messages = filter(matcher, received_messages)
 
-	print(len(list(correct_messages)))
+	print(rules)
+	# print(sub_rules)
+
 
 def part_two():
 	pass
